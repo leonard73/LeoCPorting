4baa173bb73f5c21cfa34561ae20034035518f9a
root <panjiaqi54321@qq.com>
2023-05-02 08:26:19 +0800

    clean unused files

--- Projects/chatgpt_api/scripts/genImage.sh
+++ Projects/chatgpt_api/scripts/genImage.sh
@@ -2,7 +2,7 @@ curl https://api.openai.com/v1/images/generations \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer $OPENAI_API_KEY" \
   -d '{
-    "prompt": "Please write a lovely dog and her friend leo who is a lion in Paris",
+    "prompt": "Generate an image about: the theme is using openai api in c language tutorial,the image should be well optimized in color,smooth,and charming",
     "n": 1,
     "size": "512x512"
   }'
