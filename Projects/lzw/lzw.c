feac78c2d45f4a18552cb6ac14ee1b2221231e7f
leonard73 <panjiaqi@54321@qq.com>
2023-05-18 22:42:05 +0800

    update lzw struct

--- Projects/lzw/lzw.c
+++ Projects/lzw/lzw.c
@@ -29,6 +29,51 @@ void save_data_to_file(uint8_t* data,char* path,uint32_t sz)
     fwrite(data,1,sz,fp);
     fclose(fp);
 }
+typedef struct DICTIONARY_ELEM_t
+{
+    uint8_t * data;
+    uint32_t sz;
+}DICTIONARY_ELEM;
+typedef struct DICTIONARY_t
+{
+    DICTIONARY_ELEM * dict_data_array;
+    uint32_t          sz;
+}DICTIONARY;
+typedef struct TMP_MATCH_POOL_t
+{
+    uint8_t * array_data;
+    uint32_t  sz;
+}TMP_MATCH_POOL;
+int isFindSameInDictPool(TMP_MATCH_POOL * tmpPool_p,DICTIONARY * dict_p)
+{
+    uint32_t tmpPoolSZ = tmpPool_p->sz;
+    for(uint32_t i=0;i<dict_p->sz;i++)
+    {
+        if(tmpPoolSZ == dict_p->dict_data_array[i].sz)
+        {
+            //start match elements
+            uint8_t eq=1;
+            for(uint32_t k=0;k<tmpPoolSZ;k++)
+            {
+                eq &= (tmpPool_p->array_data[k] ==  dict_p->dict_data_array[i].data[k]) ? 1 : 0;
+            }
+            if(eq==1)
+            {
+                //match
+            }
+        }
+    }
+}
+uint32_t lzw_encode(uint8_t * input,uint8_t** output,uint32_t in_sz)
+{
+    uint32_t lzw_data_sz=0;
+    TMP_MATCH_POOL tmpPool;
+    tmpPool.sz=0,tmpPool.array_data=0;
+    for(uint32_t i=0;i<in_sz;i++)
+    {
+        
+    }
+}
 int lzw_encoder(char * fileSrc,char * fileDst)
 {
     uint8_t * in_data=0,*out_data=0;
